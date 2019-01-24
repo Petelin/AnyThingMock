de681ef61ab2275ff43f7a9d0d4add923abce117
lin <petelin1120@gmail.com>
2019-01-24 18:29:03 +0800

    Create mock.go

---
+++ mock.go
@@ -0,0 +1,19 @@
+type Mock struct {
+	innerRec []func()
+}
+
+func (m *Mock) On(item interface{}, new interface{}) *Mock {
+	old := reflect.ValueOf(item).Elem().Interface()
+	rec := func() {
+		reflect.ValueOf(item).Elem().Set(reflect.ValueOf(old))
+	}
+	reflect.ValueOf(item).Elem().Set(reflect.ValueOf(new))
+	m.innerRec = append(m.innerRec, rec)
+	return m
+}
+
+func (m *Mock) Recover(item interface{}, new interface{}) {
+	for _, f := range m.innerRec {
+		f()
+	}
+}
